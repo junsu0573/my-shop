7c5ed863655c7b8b33d79165f8e441745e3bf55b
전봉식 <bongsik@jeonbongsig-ui-MacBookAir.local>
2025-08-10 19:32:04 +0900

    fix weak

--- client/src/widgets/Header.tsx
+++ client/src/widgets/Header.tsx
@@ -1,5 +1,4 @@
 import { Link, useNavigate } from "react-router-dom";
-import Input from "../shared/ui/input";
 import Button from "../shared/ui/button";
 import { User, ShoppingCart, Menu } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
