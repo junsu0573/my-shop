df679c3718aeb81c2b88d7b7b4efd472e6e4e15c
전봉식 <bongsik@jeonbongsig-ui-MacBookAir.local>
2025-08-17 03:05:33 +0900

    Add logo

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,8 +1,15 @@
 import LoginForm from "../features/auth/LoginForm";
+import logoImg from "../assets/thumbs_up_cow.svg";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
   return (
-    <div className="flex items-center justify-center bg-background min-h-screen">
+    <div className="flex flex-col items-center justify-center bg-background min-h-screen px-4">
+      <Link to="/" className="mb-6">
+        <img src={logoImg} alt="logo" className="w-30 -mb-6" />
+        <span className="text-4xl font-bold text-primary">한우마켓</span>
+      </Link>
+
       <LoginForm />
     </div>
   );
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
 import RegisterForm from "../features/auth/RegisterForm";
-
+import logoImg from "../assets/thumbs_up_cow.svg";
 function RegisterPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-background">
+      <Link to="/" className="mb-6">
+        <img src={logoImg} alt="logo" className="w-30 -mb-6" />
+        <span className="text-4xl font-bold text-primary">한우마켓</span>
+      </Link>
       <RegisterForm />
     </div>
   );
